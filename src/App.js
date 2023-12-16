d882f3e06e70922d9cc4ee250f8becc18f947ab3
Ervand Sargsyan <sargsyanervand2010.ait@gmail.com>
2023-12-16 17:07:03 +0400

    Fixed bugs in the App function
    And completely changed the Header function

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import Header from "./components/Header.jsx";
 
 export default function App() {
 	let [dogs, setDogs] = useState([]);
+	let names = randomBreeds(10, dogs);
 
 	useEffect(() => {
 		fetch(`https://dog.ceo/api/breeds/list/all`).then(res => {
-			res.json()
+			return res.json()
 		}).then(data => {
 			let breeds = Object.keys(data.message);
 			setDogs(breeds)
-		}).catch(err => { console.log(err); });
-	}, [])
+		});
+	}, []);
 
 	function randomBreeds(quantity, array) {
 		let randomNums = [];
 		let newArray = [];
+
 		for (let index = 0; index < quantity; index++) {
 			let rand = Math.floor(Math.random() * array.length);
-			// if (randomNums.indexOf(rand) === -1) {
 			randomNums.push(rand);
-			// } else {
-			// index--
-			// }
 		}
 
 		for (let index = 0; index < randomNums.length; index++) {
@@ -30,9 +28,7 @@ export default function App() {
 		}
 
 		return newArray;
-	}
-
-	let names = randomBreeds(10, dogs)
+	};
 
 	return (
 		<>
@@ -40,4 +36,3 @@ export default function App() {
 		</>
 	);
 }
-
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function Header() {
+export default function Header({ dogs }) {
+	const [activeBreeds, setActiveBreeds] = useState([]);
+	function checkedInActive(element) {
+		if (activeBreeds.find(el => el === element)) {
+			const actives = activeBreeds.filter((elem, index) => elem !== element);
+			setActiveBreeds([...activeBreeds, actives]);
+		} else {
+			const actives = dogs.filter((e, i) => e === element)[0];
+			setActiveBreeds([...activeBreeds, actives]);
+		}
+	}
+
+	function getId(element) {
+		return activeBreeds.find((el) => el === element)
+	}
+
 	return (
 		<header className="flex flex-col items-center justify-center w-[100%]">
 			<div className="flex items-center justify-center pb-[35px] pt-[35px] border-b-2 border-solid border-black w-[100%]">
@@ -10,7 +25,19 @@ export default function Header() {
 				</h1>
 			</div>
 			<nav className="flex items-center justify-center w-[100%] border-b-2 pb-[15px] pt-[15px] border-solid border-black text-[20px]">
-
+				<ul className='flex justify-evenly w-full'>
+					{dogs.map((element, index) => {
+						return (
+							<li
+								key={`item${index}`}
+								className={getId(element) ? "border border-black cursor-pointer rounded-md h-[38px] pl-[6px] pr-[6px] flex justify-center items-center" : "cursor-pointer h-[38px] flex justify-center items-center pl-[7px] pr-[7px]"}
+								onClick={() => checkedInActive(element)}
+							>
+								{element}
+							</li>
+						)
+					})}
+				</ul>
 			</nav>
 		</header>
 	)
